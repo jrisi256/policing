4d4d6318e7803b400c3b54aba7c8609a38f64a9f
jrisi256 <jrisi256@gmail.com>
2022-03-31 23:47:09 -0400

    Added some more details to the schemas. Began replicating Ba. Commented code. Updated lockfile. Added in some old files which will be helpful for replicating.

--- functions.R
+++ functions.R
@@ -1,12 +1,15 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(purrr)
 library(stringr)
 
+# Takes in a file path and returns a data frame with the correct column types.
 my_read_csv <- function(file_path) {
     
     df <- read_csv(file_path)
     
+    # Factors, characters, logical. Everything not mentioned uses the default.
     f <- "race|gender|district|rank|weekday|unit|shift|type|code"
     c <- "id|lat|lon"
     l <- "spanish|injured|first"
@@ -17,12 +20,14 @@ my_read_csv <- function(file_path) {
                across(matches(l), function(col) {as.logical(col)}))
 }
 
+# Summarize a column
 GetSummaryCol <- function(df, col) {
     
     if(is.numeric(df[[col]])) {
         
         q <- quantile(df[[col]], na.rm = T)
         
+        # Numeric columns
         df %>%
             transmute(col = col,
                       min = min(.data[[col]], na.rm = T),
@@ -41,6 +46,7 @@ GetSummaryCol <- function(df, col) {
         
     } else if(is.factor(df[[col]])) {
         
+        # Factors
         df %>%
             group_by(.data[[col]]) %>%
             summarise(n = n()) %>%
@@ -52,6 +58,7 @@ GetSummaryCol <- function(df, col) {
         
     } else if(is.logical(df[[col]])) {
         
+        # Logical
         df %>%
             transmute(col = col,
                       mean = mean(.data[[col]]),
@@ -62,22 +69,31 @@ GetSummaryCol <- function(df, col) {
             distinct()
         
     } else {
+        
+        # Everything not a factor, numeric, or a logical
         df %>%
             transmute(col = col,
                       missing = sum(is.na(.data[[col]])),
+                      nr_unique = length(unique(.data[[col]])),
                       prcnt_missing = missing / nrow(df)) %>%
             distinct()
     }
 }
 
+# Given a data frame, summarise each column
 GetSummary <- function(df) {
 
+    # Get the column type for each column.
+    # Some columns return more than one type which is why I need to subset.
+    # E.g. POSIXct
     col_types <- unlist(map(df, function(col) {class(col)[1]}))
     cols <- colnames(df)
     names(cols) <- col_types
     
+    # Returns a data frame for each column.
     summary <- map(cols, GetSummaryCol, df = df)
     
+    # Bind each data frame together of the same variable type.
     numeric <- bind_rows(summary[names(summary) == "numeric"])
     factor <- bind_rows(summary[names(summary) == "factor"])
     logical <- bind_rows(summary[names(summary) == "logical"])
@@ -89,6 +105,8 @@ GetSummary <- function(df) {
                       "logical" = logical,
                       "other" = other)
     
+    # Delete any entries with no data frames in them.
+    # Happens when the data frame does not have any columns of a specific type.
     if(length(summaries[["numeric"]]) == 0) {
         summaries[["numeric"]] <- NULL
     } else if(length(summaries[["factor"]]) == 0) {
